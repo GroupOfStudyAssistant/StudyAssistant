848c72d274c657828fe159f9cedbc9d2475a09a9
hwc <hwangwc@qq.com>
2020-02-15 20:07:59 +0800

    virtualenv(to unify our environment)

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, url_for
+
+
 app = Flask(__name__)
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('welcome.html')
 
