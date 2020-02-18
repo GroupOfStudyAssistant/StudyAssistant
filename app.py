3a71d9f84025b0350aac17530b850a811672fe40
hwc <hwangwc@qq.com>
2020-02-18 18:26:13 +0800

    neterror tem_save

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 import os
 import click
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'\
@@ -13,16 +14,45 @@ db = SQLAlchemy(app)
 class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
     id = db.Column(db.Integer, primary_key=True)  # 主键
     name = db.Column(db.String(20))  # 名字
+    username = db.Column(db.String(20)) # 用户名 可考虑设为单一值
+    password_hash = db.Column(db.String(128)) # 密码散列
+
+    # 密码与验证
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 
 #路由修饰器
+# 该路由仅用于展示,完成跳转到登录界面和注册界面的功能
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # 该路由仅用于展示,完成跳转到登录界面和注册界面的功能
     return render_template('index.html')
 
+#注册功能
+@app.route('/signup', methods = ['GET', 'POST'])
+def signup():
+
+    return render_template('signup.html')
+#登录功能
+@app.route('/signin', methods = ['GET', 'POST'])
+def signin():
+
+    return render_template('signin.html')
+#展示
+@app.route('/welcome')
+def welcome():
+
+    return render_template('welcome.html')
+
 #代码修饰器
+
+
+
+
 
 #模板上下文
 @app.context_processor
@@ -60,4 +90,3 @@ def forge():
 
     db.session.commit()
     click.echo('Done')
-
