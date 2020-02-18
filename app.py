94f5d275d7ef99317496adfff92cc158d9e51bc6
hwc <hwangwc@qq.com>
2020-02-18 10:39:48 +0800

    fake data for test

--- app.py
+++ app.py
@@ -16,13 +16,15 @@ class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
 
 
 
-
+#路由修饰器
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    # 该路由仅用于展示,完成跳转到登录界面和注册界面的功能
     return render_template('index.html')
 
 
 # 注册flask命令
+# 自动创建数据库
 @app.cli.command()
 @click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
 def initdb(drop):
@@ -31,3 +33,21 @@ def initdb(drop):
         db.drop_all()
     db.create_all()
     click.echo('Initialized database.')  # 输出提示信息
+
+@app.cli.command()
+def forge():
+    #生成测试数据
+    db.create_all()
+    users = [
+        {'name':'FanYx'},
+        {'name':'HuangWc'},
+        {'name':'HuWb'}
+    ]
+
+    for u in users:
+        user = User(name = u['name'])
+        db.session.add(user)
+
+    db.session.commit()
+    click.echo('Done')
+
