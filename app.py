584c5cc8d28538292a6cbcd42d6563439572eaa7
hwc <hwangwc@qq.com>
2020-02-19 17:15:27 +0800

    signin

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
-from flask import Flask, render_template, url_for, request, redirect, flash
+from flask import Flask, render_template, url_for, \
+    request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 import os
 import click
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user
 
+
+#应用设置
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dev'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'\
     + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控(暂时不知道用途)
+# 拓展类
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+@login_manager.user_loader
+def load_user(user_id): #用户加载回调函数,具体意义不明
+    user = User.query.get(int(user_id))
+    return user
 
 #数据库模型
-class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+class User(db.Model, UserMixin):  # 表名将会是 user（自动生成，小写处理）
     id = db.Column(db.Integer, primary_key=True)  # 主键
     username = db.Column(db.String(20)) # 用户名 
     password_hash = db.Column(db.String(128)) # 密码散列
@@ -39,6 +50,10 @@ def signup():
         username = request.form.get('user_name')
         password = request.form.get('user_password')
         password1 = request.form.get('user_password_1')
+        userexist = User.query.filter_by(username = username).count()
+        if userexist: #用户名已经存在
+            flash('Username Occupied!')
+            return redirect(url_for('signup'))
         if password!=password1: #密码验证失败
             flash('Two passwords do not match')
             return redirect(url_for('signup')) #重定向为注册页(可改进保留用户名)
@@ -55,8 +70,24 @@ def signup():
 #登录功能
 @app.route('/signin', methods = ['GET', 'POST'])
 def signin():
+    if request.method == 'POST':
+        username = request.form['user_name']
+        password = request.form['user_password']
+        if not username or not password:
+            flash('Invalid input.')
+            return redirect(url_for('signin'))
+        
+        user = User.query.filter_by(username = username).first()
+        #验证密码
+        if username == user.username and user.validate_password(password):
+            login_user(user) #登入
+            flash('Login success.')
+            return redirect(url_for('welcome'))
+        flash('Invalid username or password.')
+        return redirect(url_for('signin'))
 
     return render_template('signin.html')
+
 #展示
 @app.route('/welcome')
 def welcome():
