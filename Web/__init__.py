26129e8aead15ced2429f4bf1610519e5e417a35
hwc <hwangwc@qq.com>
2020-05-21 21:32:49 +0800

    hwc update web

--- Web/__init__.py
+++ Web/__init__.py
@@ -1,22 +1,28 @@
-import os
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+
+# 操作系统以及运行时环境
+import os 
 import sys
 
+# flask
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+
+# 功能拓展
 from elasticsearch import Elasticsearch
 from BuildIndex.app import GraphSearch
 
 # 应用设置
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dev'
+app.config['SECRET_KEY'] = 'dev' 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'\
-    + os.path.join(os.path.dirname(app.root_path), 'data.db')
+    + os.path.join(os.path.dirname(app.root_path), 'data.db') 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控, 否则太多警告信息
 
-# 拓展类
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy(app) # 实例化orm拓展
+login_manager = LoginManager(app) # 实例化登录管理
 login_manager.login_view = 'signin' #未登录视图保护的重定向端点
 
 # ES实例
@@ -26,7 +32,7 @@ graph = GraphSearch()
 
 
 @login_manager.user_loader
-def load_user(user_id): #用户加载回调函数,具体意义不明
+def load_user(user_id): #用户加载回调函数,将当前用户实例存入current_user变量中.
     from Web.models import User
     user = User.query.get(int(user_id))
     return user
--- Web/views.py
+++ Web/views.py
@@ -1,9 +1,13 @@
+# 视图函数文件
+
 from flask import render_template, url_for, request, redirect, flash
 from flask_login import login_user, login_required, logout_user, current_user
 
-from Web import app, db, es, graph
+from Web import app, db
 from Web.models import User
 import pymysql, json
+
+
 
 # 该路由仅用于展示,完成跳转到登录界面和注册界面的功能
 @app.route('/', methods=['GET', 'POST'])
@@ -13,22 +17,23 @@ def index():
 #注册功能 警告信息用javascript实现更合适
 @app.route('/signup', methods = ['GET', 'POST'])
 def signup():
-    if request.method == 'POST': # 如果是提交表单
+    if request.method == 'POST': # 提交表单会发送POST请求
         username = request.form.get('user_name')
         password = request.form.get('user_password')
         password1 = request.form.get('user_password_1')
 
-        if not username or not password or not password1:
+        if not username or not password or not password1: # 某字段为空的处理
             flash('Invalid input.')
-            return redirect(url_for('signup'))       
+            return redirect(url_for('signup'))
 
-        userexist = User.query.filter_by(username = username).count()
-        if userexist: #用户名已经存在
+        userexist = User.query.filter_by(username = username).count() # 判断用户是否存在
+        if userexist:
             flash('Username Occupied!')
             return redirect(url_for('signup'))
         if password!=password1: #密码验证失败
             flash('Two passwords do not match.')
             return redirect(url_for('signup')) #重定向为注册页(可改进保留用户名)
+        
         # 保存到数据库
         user = User(username = username)
         user.set_password(password)
@@ -47,125 +52,25 @@ def signin():
     if current_user.is_authenticated:
         return redirect(url_for('welcome'))
 
-    if request.method == 'POST':
+    if request.method == 'POST': # 提交表单会发送POST请求
         username = request.form['user_name']
         password = request.form['user_password']
         if not username or not password:
             flash('Invalid input.')
             return redirect(url_for('signin'))
         
-        user = User.query.filter_by(username = username).first()
         userexist = User.query.filter_by(username = username).count()
+        user = User.query.filter_by(username = username).first()
         #验证密码
         if userexist == 1 and username == user.username and user.validate_password(password):
             login_user(user) #登入
             flash('Login success.')
             return redirect(url_for('welcome'))
+
         flash('Invalid username or password.')
         return redirect(url_for('signin'))
 
     return render_template('signin.html')
-
-# 这一部分还没有改太好
-def get_concepts(textforsearch):
-    key = "_".join(x.lower() for x in textforsearch.split())
-    dsl_concept_1 = {
-        'query': {
-            'multi_match': {
-                'query': key,
-                'fields': ['concept', 'definition']
-            }
-        }
-    }
-    qresult_concept = es.search(index="conceptlist", body=dsl_concept_1)
-    concepts = qresult_concept["hits"]["hits"][0:min(3, len(qresult_concept["hits"]["hits"]))]
-    
-    """
-    if qresult_concept["hits"]["hits"][0]["concept"].lower != key:
-        dsl_concept_2 = {
-            'query': {
-                'multi_match': {
-                    'query': textforsearch,
-                    'fields': ['concept', 'definition']
-                }
-            }
-        }
-        qresult_concept = es.search(index="conceptlist", body=dsl_concept)
-        concepts = qresult_concept["hits"]["hits"][0:min(3, len(qresult_concept["hits"]["hits"]))]
-    """
-    return concepts
-
-def get_relations(textforsearch):
-    relations = graph.getAll(textforsearch)
-    return json.dumps(relations)
-
-def get_moocs(textforsearch):
-    dsl_mooc = {
-        'query': {
-            'multi_match': {
-                'query': textforsearch,
-                'fields': ['name', 'blackboard']
-            }
-        }
-    }
-    qresult_mooc = es.search(index="mooc", body=dsl_mooc)
-    moocs = qresult_mooc["hits"]["hits"][0:min(5, len(qresult_mooc["hits"]["hits"]))]
-    return moocs
-
-# 对自己爬取的部分课程
-def get_prereqs(textforsearch):
-    dsl_uni_course = {
-        'query': {
-            'match_phrase': {'name': textforsearch} # 短语匹配可以让短语不分开
-        }
-    }
-    qresult_course = es.search(index="uni_course", body = dsl_uni_course)
-    if qresult_course["hits"]["total"]["value"]: # match_phrase有结果的话
-        print("hits num:", qresult_course["hits"]["total"]["value"])
-        course = qresult_course["hits"]["hits"][0] # 暂时取第一个作为展示的结果图，后续可以筛选。
-    else: # 否则，重新按照match查询
-        dsl_uni_course = {
-            'query': {
-                'multi_match': {
-                    'query': textforsearch,
-                    'fields': ['name', 'descript']}
-            }
-        }
-        qresult_course = es.search(index="uni_course", body = dsl_uni_course)
-        if qresult_course["hits"]["total"]["value"]:
-            print("hits num:", qresult_course["hits"]["total"]["value"])
-            course = qresult_course["hits"]["hits"][0]# 暂时取第一个作为展示的结果图，后续可以筛选。
-        else:
-            course = {"_source": {}}
-    with open('E:/KGProject/StudyAssistant/Web/resources/result.json', 'w') as f: # 需替换成自己的路径
-        json.dump(course["_source"]["children"], f) # 读取时使用json.load(f)
-    return course
-
-#展示
-@app.route('/welcome', methods = ['GET', 'POST'])
-@login_required
-def welcome():
-    # 没有搜索动作时的页面展示
-    textforsearch = request.args.get('search', '') #获取查询参数search的值, 默认为空
-    concepts = [{'_source': {'concept': '机器学习', 'definition': '''机器学习是一门多领域交叉学科，涉及概率论、统计学、逼近论、凸分析、算法复杂度理论等多门学科。
-        专门研究计算机怎样模拟或实现人类的学习行为，以获取新的知识或技能，重新组织已有的知识结构使之不断改善自身的性能。
-        它是人工智能的核心，是使计算机具有智能的根本途径。'''}}]
-    relations = []
-    moocs = []
-    prereqs = {"_source": {}}
-    if textforsearch != '':
-        # 进行查询等处理.可写成一个函数.
-        print("get text: "+textforsearch)
-    if request.method == 'POST': #post方法说明用户提交了查询
-        textforsearch = request.form.get("keywords")
-        if not textforsearch:
-            return redirect(url_for('welcome'))
-        concepts = get_concepts(textforsearch)
-        relations = get_relations(textforsearch)
-        #print(relations)
-        moocs = get_moocs(textforsearch)
-        prereqs = get_prereqs(textforsearch) # 得到的是我们认为和搜索词最接近的uni_course的数据
-    return render_template('welcome.html', concepts = concepts, relations = relations, moocs = moocs, textforsearch = textforsearch, prereqs = prereqs)
 
 # 相当于使用一个路由完成登出的功能,比较浪费,最好用js做
 @app.route('/signout')
@@ -175,4 +80,33 @@ def signout():
     flash('Signout success.')
     return redirect(url_for('index'))
 
+
+
+
+######## 核心功能
+from Web.proc_txt4srch import get_concepts,get_relations,get_moocs,get_prereqs
+#搜索页
+@app.route('/welcome', methods = ['GET', 'POST']) # 允许使用get和post方式发送搜索请求.
+@login_required  #可以考虑把用户登陆限制去掉
+def welcome(): 
+    concepts = [{'_source': {'concept': '机器学习', 'definition': '''机器学习是一门多领域交叉学科，涉及概率论、统计学、逼近论、凸分析、算法复杂度理论等多门学科。
+        专门研究计算机怎样模拟或实现人类的学习行为，以获取新的知识或技能，重新组织已有的知识结构使之不断改善自身的性能。
+        它是人工智能的核心，是使计算机具有智能的根本途径。'''}}]
+    relations = []
+    moocs = []
+    prereqs = {"_source": {}}
+
+    if request.method == 'POST':  # POST请求表示提交表单
+        textforsearch = request.form.get("keywords")
+    else: # get请求从requests中获取参数
+        textforsearch = request.args.get('search', '') # 默认值为空
+
+    if textforsearch.strip(): # 如果不是是空白字符才进行检索处理
+        concepts = get_concepts(textforsearch)
+        relations = get_relations(textforsearch)
+        #print(relations)
+        moocs = get_moocs(textforsearch)
+        prereqs = get_prereqs(textforsearch) # 得到的是我们认为和搜索词最接近的uni_course的数据
+
+    return render_template('welcome.html', concepts = concepts, relations = relations, moocs = moocs, textforsearch = textforsearch, prereqs = prereqs)
 
